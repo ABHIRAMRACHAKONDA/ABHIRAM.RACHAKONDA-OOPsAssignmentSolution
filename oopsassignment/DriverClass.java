76e7c5f25d524d7dec44ddfdb28bdb648b506dc0
ABHIRAMRACHAKONDA <68802081+ABHIRAMRACHAKONDA@users.noreply.github.com>
2022-06-19 13:30:16 +0530

    Update DriverClass.java

--- oopsassignment/DriverClass.java
+++ oopsassignment/DriverClass.java
@@ -1,31 +1,21 @@
-package oopsassignment;
+package com.oopsassignment;
 
 public class DriverClass {
 
 	public static void main(String[] args) {
-		
+
 		HrDepartment hrDepartment = new HrDepartment();
-        TechDepartment techDepartment = new TechDepartment();
-        AdminDepartment adminDepartment = new AdminDepartment();
-        
-        System.out.println(adminDepartment.departmentName());
-        System.out.println(adminDepartment.getTodaysWork());
-        System.out.println(adminDepartment.getWorkDeadline());
-        System.out.println(adminDepartment.isTodayAHoliday());
-        System.out.println();
-        
-        System.out.println(hrDepartment.departmentName());
-        System.out.println(hrDepartment.doActivity());
-        System.out.println(hrDepartment.getTodaysWork());
-        System.out.println(hrDepartment.getWorkDeadline());
-        System.out.println(hrDepartment.isTodayAHoliday());
-        System.out.println();
-        
-        System.out.println(techDepartment.departmentName());
-        System.out.println(techDepartment.getTodaysWork());
-        System.out.println(techDepartment.getWorkDeadline());
-        System.out.println(techDepartment.getTechStackInformation());
-        System.out.println(techDepartment.isTodayAHoliday());
+		TechDepartment techDepartment = new TechDepartment();
+		AdminDepartment adminDepartment = new AdminDepartment();
+
+		System.out.println(adminDepartment.departmentName() + "\n" + adminDepartment.getTodaysWork() + "\n"
+				+ adminDepartment.getWorkDeadline() + "\n" + adminDepartment.isTodayAHoliday() + "\n" + "\n"
+				+ hrDepartment.departmentName() + "\n" + hrDepartment.doActivity() + "\n" + hrDepartment.getTodaysWork()
+				+ "\n" + hrDepartment.getWorkDeadline() + "\n" + hrDepartment.isTodayAHoliday() + "\n" + "\n"
+				+ techDepartment.departmentName() + "\n" + techDepartment.getTodaysWork() + "\n"
+				+ techDepartment.getWorkDeadline() + "\n" + techDepartment.getTechStackInformation() + "\n"
+				+ techDepartment.isTodayAHoliday());
+
 	}
 
 }
